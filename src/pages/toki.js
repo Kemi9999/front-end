9ca175283fa9876dda574307dcb363273a6f719d
Kemi9999 <temuujinbatchuluun9@gmail.com>
2025-04-18 13:27:23 +0800

    push

---
+++ src/pages/toki.js
@@ -0,0 +1,10 @@
+export default function Toki() {
+    return (
+        <div className="w-screen h-screen bg-slate-300">
+            <div className="w-full h-20 bg-white flex">
+                <img className="w-30 h-14 object-cover flex" src="https://www.toki.mn/wp-content/uploads/2022/08/Asset-1@3x.png"></img>
+                <p>Үйлчилгээ</p>
+                 </div>
+        </div>
+    )
+}
